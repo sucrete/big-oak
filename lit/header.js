ee7e2577394f8fa01fa9662b70e72fb2a7d8ce29
Will Andree <williamandree@gmail.com>
2025-04-23 12:42:10 -0500

    Working on honme page. Add padding to bottom of inner body.

--- lit/header.js
+++ lit/header.js
@@ -32,7 +32,7 @@ export class Header extends LitElement {
                     </li>
 
                     <li>
-                      <a class="nav-link" href="#"> Another Page</a>
+                      <a class="nav-link" href="#">Course History</a>
                     </li>
                   </ul>
                 </nav>
