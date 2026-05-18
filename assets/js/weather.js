ac09553b0320f25bd3c6ae6ca01a6b99d182f331
Will Andree <williamandree@gmail.com>
2026-05-18 13:55:12 -0500

    Updating weather widget.

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,9 +1,9 @@
-//~ Standard Variables
-// API URL
 const apiUrl =
-  "https://api.pirateweather.net/forecast/EqRw5datS5zL99ze3FwQ8Q7PJEtJAC0i/42.907,-76.963?exclude=minutely,hourly,alerts,flags";
+  "https://api.open-meteo.com/v1/forecast?latitude=42.907&longitude=-76.963" +
+  "&current=temperature_2m,wind_speed_10m,weather_code,is_day" +
+  "&daily=weather_code,temperature_2m_max,temperature_2m_min" +
+  "&temperature_unit=fahrenheit&wind_speed_unit=mph&timezone=America/New_York&forecast_days=5";
 
-// Days of the week
 const days = [
   "Sunday",
   "Monday",
@@ -16,7 +16,6 @@ const days = [
 
 var weatherData;
 
-//~ First call Pirate Weather API
 async function getWeather() {
   await fetch(apiUrl, { cache: "no-store" })
     .then((response) => {
@@ -26,12 +25,8 @@ async function getWeather() {
       return response.json();
     })
     .then((data) => {
-      // console.log(JSON.stringify(data, null, 2));
-      // code below gets day of the week
-      console.log(
-        `%c${days[new Date(data.currently.time * 1000).getDay()]}`,
-        "color:green"
-      );
+      const dayName = days[new Date(data.current.time).getDay()];
+      console.log(`%c${dayName}`, "color:green");
       weatherData = data;
       return data;
     })
@@ -40,19 +35,14 @@ async function getWeather() {
     });
 }
 
-//~ Begin DOM manipulation - fired when fetch to API is finished - receives data object
 async function populateDOM() {
   await getWeather();
-  // now you can directly use jsonData
 
-  //~ grab all DOM elements
-  // today
   const weatherIcon = document.getElementsByClassName("icon-img")[0];
   const theTemp = document.getElementsByClassName("the-temp")[0];
   const weather = document.getElementsByClassName("what-weather")[0];
   const windSpeed = document.getElementsByClassName("wind")[0];
 
-  // tomorrow
   const tomorrowDay = document.getElementsByClassName("tomorrow")[0];
   const tomorrowWeatherIcon =
     document.getElementsByClassName("tomorrow-icon")[0];
@@ -63,7 +53,6 @@ async function populateDOM() {
     .getElementsByClassName("future-row-1")[0]
     .getElementsByClassName("lo")[0];
 
-  // day after tomorrow
   const dayAfterTomorrowDay =
     document.getElementsByClassName("day-after-tomorrow")[0];
   const dayAfterTomorrowIcon = document.getElementsByClassName(
@@ -76,7 +65,6 @@ async function populateDOM() {
     .getElementsByClassName("future-row-2")[0]
     .getElementsByClassName("lo")[0];
 
-  // three days from today
   const threeDaysFromTodayDay = document.getElementsByClassName(
     "three-days-from-today"
   )[0];
@@ -90,72 +78,65 @@ async function populateDOM() {
     .getElementsByClassName("future-row-3")[0]
     .getElementsByClassName("lo")[0];
 
-  //~ Set Today's Weather
-  // console.log(weatherData.currently.icon);
-  const today = weatherData.currently;
-  // set today's icon
-  const iconPath = assignIcon(today.icon);
-  weatherIcon.src = `./assets/images/icons/${iconPath}.png`;
+  const current = weatherData.current;
+  const daily = weatherData.daily;
 
-  // set today's temperature readable
-  theTemp.innerHTML = Math.ceil(today.temperature);
+  weatherIcon.src = `./assets/images/icons/${assignIcon(current.weather_code, current.is_day)}.png`;
+  theTemp.innerHTML = Math.ceil(current.temperature_2m);
+  weather.innerHTML = weatherDescription(current.weather_code);
+  windSpeed.innerHTML = Math.ceil(current.wind_speed_10m);
 
-  // set today's weather readable
-  weather.innerHTML = today.summary;
+  const tomorrow = 1;
+  tomorrowDay.innerHTML = days[new Date(daily.time[tomorrow]).getDay()];
+  tomorrowWeatherIcon.src = `./assets/images/icons/${assignIcon(daily.weather_code[tomorrow])}.svg`;
+  tomorrowHi.innerHTML = Math.ceil(daily.temperature_2m_max[tomorrow]);
+  tomorrowLo.innerHTML = Math.ceil(daily.temperature_2m_min[tomorrow]);
 
-  // set today's wind speed
-  windSpeed.innerHTML = Math.ceil(today.windSpeed);
+  const dat = 2;
+  dayAfterTomorrowDay.innerHTML = days[new Date(daily.time[dat]).getDay()];
+  dayAfterTomorrowIcon.src = `./assets/images/icons/${assignIcon(daily.weather_code[dat])}.svg`;
+  dayAfterTomorrowHi.innerHTML = Math.ceil(daily.temperature_2m_max[dat]);
+  dayAfterTomorrowLo.innerHTML = Math.ceil(daily.temperature_2m_min[dat]);
 
-  //~ set tomorrow's Weather
-  const tomorrow = weatherData.daily.data[2];
-  tomorrowDay.innerHTML = days[new Date(tomorrow.time * 1000).getDay()];
-  tomorrowWeatherIcon.src = `./assets/images/icons/${assignIcon(
-    tomorrow.icon
-  )}.svg`;
-  tomorrowHi.innerHTML = Math.ceil(tomorrow.temperatureHigh);
-  tomorrowLo.innerHTML = Math.ceil(tomorrow.temperatureLow);
-
-  //~ set day after tomorrow's Weather
-  const DAT = weatherData.daily.data[3];
-  dayAfterTomorrowDay.innerHTML = days[new Date(DAT.time * 1000).getDay()];
-  dayAfterTomorrowIcon.src = `./assets/images/icons/${assignIcon(
-    DAT.icon
-  )}.svg`;
-  dayAfterTomorrowHi.innerHTML = Math.ceil(DAT.temperatureHigh);
-  dayAfterTomorrowLo.innerHTML = Math.ceil(DAT.temperatureLow);
-
-  //~ set three days from today's Weather
-  const thirdOut = weatherData.daily.data[4];
-  threeDaysFromTodayDay.innerHTML =
-    days[new Date(thirdOut.time * 1000).getDay()];
-  threeDaysFromTodayIcon.src = `./assets/images/icons/${assignIcon(
-    thirdOut.icon
-  )}.svg`;
-  threeDaysFromTodayHi.innerHTML = Math.ceil(thirdOut.temperatureHigh);
-  threeDaysFromTodayLo.innerHTML = Math.ceil(thirdOut.temperatureLow);
+  const third = 3;
+  threeDaysFromTodayDay.innerHTML = days[new Date(daily.time[third]).getDay()];
+  threeDaysFromTodayIcon.src = `./assets/images/icons/${assignIcon(daily.weather_code[third])}.svg`;
+  threeDaysFromTodayHi.innerHTML = Math.ceil(daily.temperature_2m_max[third]);
+  threeDaysFromTodayLo.innerHTML = Math.ceil(daily.temperature_2m_min[third]);
 }
 
-getWeather();
 populateDOM();
 
-const assignIcon = function (icon) {
-  // console.log(`%c${icon}`, "color: orange");
-  let iconSrc;
-  if (icon == "clear-day" || icon == "clear-night") {
-    iconSrc = "sun";
-  } else if (icon == "rain") {
-    iconSrc = "rain";
-  } else if (icon == "snow" || icon == "sleet") {
-    iconSrc = "snow";
-  } else if (icon == "wind") {
-    iconSrc = "wind";
-  } else if (icon == "fog") {
-    iconSrc = "foggy";
-  } else if (icon == "cloudy") {
-    iconSrc = "cloudy";
-  } else if (icon == "partly-cloudy-day" || icon == "partly-cloudy-night") {
-    iconSrc = "part-cloud";
-  }
-  // console.log(`%c${iconSrc}`, "color: yellow");
-  return iconSrc;
+// Maps WMO weather codes to icon filenames
+const assignIcon = function (code, isDay = true) {
+  if (code === 0 || code === 1) return isDay ? "sun" : "sun";
+  if (code === 2) return "part-cloud";
+  if (code === 3) return "cloudy";
+  if (code === 45 || code === 48) return "foggy";
+  if (code >= 51 && code <= 67) return "rain";
+  if (code >= 71 && code <= 77) return "snow";
+  if (code >= 80 && code <= 82) return "rain";
+  if (code === 85 || code === 86) return "snow";
+  if (code >= 95) return "rain";
+  return "sun";
+};
+
+// Maps WMO weather codes to human-readable descriptions
+const weatherDescription = function (code) {
+  if (code === 0) return "Clear";
+  if (code === 1) return "Mostly Clear";
+  if (code === 2) return "Partly Cloudy";
+  if (code === 3) return "Overcast";
+  if (code === 45 || code === 48) return "Foggy";
+  if (code === 51 || code === 53 || code === 55) return "Drizzle";
+  if (code === 56 || code === 57) return "Freezing Drizzle";
+  if (code === 61 || code === 63 || code === 65) return "Rain";
+  if (code === 66 || code === 67) return "Freezing Rain";
+  if (code === 71 || code === 73 || code === 75) return "Snow";
+  if (code === 77) return "Snow Grains";
+  if (code >= 80 && code <= 82) return "Rain Showers";
+  if (code === 85 || code === 86) return "Snow Showers";
+  if (code === 95) return "Thunderstorm";
+  if (code === 96 || code === 99) return "Thunderstorm & Hail";
+  return "Unknown";
 };
